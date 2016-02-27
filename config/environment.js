3c7c9803d7b750be4a7ea4fc048e57ffa4e1ae15
Alex Horan <a_horan3@knights.ucf.edu>
2016-02-27 11:37:03 -0500

    initial commit

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'bookcase',
+    modulePrefix: 'bookshelf',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
